a72c22f925960081d4f3c70f76cd28503c8f6f81
Luis Bellido Triana <luis.bellido@upm.es>
2022-11-13 19:01:01 +0100

    Complete beta version
    Still need to remove interface ifconfig commands from helm charts

--- vcpe-1.sh
+++ vcpe-1.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-./vcpe_start.sh vcpe-1 10.255.0.1 10.255.0.2 192.168.255.1 10.2.3.1 conf/dhcpd-1.conf
+export OSMNS  # needs to be defined in calling shell
+export VACCESS=$VACC1
+export VCPE=$VCPE1
+
+# call vcpe_start <vnf_tunnel_ip> <home_tunnel_ip> <vcpe_private_ip> <vcpe_public_ip> <vcpe_gw> <dhcpd_conf_file>
+./vcpe_start.sh 10.255.0.1 10.255.0.2 192.168.255.1 10.100.1.1 10.100.1.254 conf/dhcpd-1.conf
--- vcpe_start.sh
+++ vcpe_start.sh
@@ -3,85 +3,85 @@
 USAGE="
 Usage:
     
-vcpe_start <vcpe_name> <vnf_tunnel_ip> <home_tunnel_ip> <vcpe_private_ip> <vcpe_public_ip> <dhcpd_conf_file>
+vcpe_start <vnf_tunnel_ip> <home_tunnel_ip> <vcpe_private_ip> <vcpe_public_ip> <vcpe_gw> <dhcpd_conf_file>
     being:
-        <vcpe_name>: the name of the network service instance in OSM 
         <vnf_tunnel_ip>: the ip address for the vnf side of the tunnel
         <home_tunnel_ip>: the ip address for the home side of the tunnel
         <vcpe_private_ip>: the private ip address for the vcpe
-        <vcpe_public_ip>: the public ip address for the vcpe (10.2.2.0/24)
+        <vcpe_public_ip>: the public ip address for the vcpe 
+        <vcpe_gw>: the default gateway for the vcpe
         <dhcpd_conf_file>: the dhcp file for the vcpe to give private addresses to the home network
 "
 
-if [[ $# -ne 6 ]]; then
-        echo ""       
-    echo "ERROR: incorrect number of parameters"
-    echo "$USAGE"
-    exit 1
-fi
+#if [[ $# -ne 6 ]]; then
+#        echo ""       
+#    echo "ERROR: incorrect number of parameters"
+#    echo "$USAGE"
+#    exit 1
+#fi
 
-VNF1="mn.dc1_$1-1-ubuntu-1"
-VNF2="mn.dc1_$1-2-ubuntu-1"
+set -u # to verify variables are defined
+: $OSMNS
+: $VACCESS
+: $VCPE
 
-VNFTUNIP="$2"
-HOMETUNIP="$3"
-VCPEPRIVIP="$4"
-VCPEPUBIP="$5"
+VNFTUNIP="$1"
+HOMETUNIP="$2"
+VCPEPRIVIP="$3"
+VCPEPUBIP="$4"
+VCPEGW="$5"
 DHCPDCONF="$6"
 
-ETH11=`sudo docker exec -it $VNF1 ifconfig | grep eth1 | awk '{print $1}'`
-ETH21=`sudo docker exec -it $VNF2 ifconfig | grep eth1 | awk '{print $1}'`
-IP11=`sudo docker exec -it $VNF1 hostname -I | awk '{printf "%s\n", $1}{print $2}' | grep 192.168.100`
-IP21=`sudo docker exec -it $VNF2 hostname -I | awk '{printf "%s\n", $1}{print $2}' | grep 192.168.100`
 
-##################### VNFs Settings #####################
-## 0. Iniciar el Servicio OpenVirtualSwitch en cada VNF:
-echo "--"
-echo "--OVS Starting..."
-sudo docker exec -it $VNF1 /usr/share/openvswitch/scripts/ovs-ctl start
-sudo docker exec -it $VNF2 /usr/share/openvswitch/scripts/ovs-ctl start
+## 1. Obtener IPs de las VNFs
+echo "## 1. Obtener IPs de las VNFs"
+IPACCESS=`microk8s kubectl exec -n $OSMNS $VACCESS -- hostname -I | awk '{print $1}'`
+echo "IPACCESS = $IPACCESS"
 
-echo "--"
-echo "--Connecting vCPE service with AccessNet and ExtNet..."
+IPCPE=`microk8s kubectl exec -n $OSMNS $VCPE -- hostname -I | awk '{print $1}'`
+echo "IPCPE = $IPCPE"
 
-sudo ovs-docker add-port AccessNet veth0 $VNF1
-sudo ovs-docker add-port ExtNet veth0 $VNF2
 
-echo "--"
-echo "--Setting VNF..."
-echo "--"
-echo "--Bridge Creating..."
+## 2. Iniciar el Servicio OpenVirtualSwitch en cada VNF:
+echo "## 2. Iniciar el Servicio OpenVirtualSwitch en cada VNF"
+microk8s kubectl exec -n $OSMNS $VACCESS -- service openvswitch-switch start
+microk8s kubectl exec -n $OSMNS $VCPE -- service openvswitch-switch start
 
-## 1. En VNF:vclass agregar un bridge y asociar interfaces.
-sudo docker exec -it $VNF1 ovs-vsctl add-br br0
-sudo docker exec -it $VNF1 ifconfig veth0 $VNFTUNIP/24
-sudo docker exec -it $VNF1 ovs-vsctl add-port br0 vxlan1 -- set interface vxlan1 type=vxlan options:remote_ip=$HOMETUNIP
-sudo docker exec -it $VNF1 ovs-vsctl add-port br0 vxlan2 -- set interface vxlan2 type=vxlan options:remote_ip=$IP21
+## 3. En VNF:vclass agregar un bridge y asociar interfaces
+echo "## 3. En VNF:vclass agregar un bridge y asociar interfaces"
+microk8s kubectl exec -n $OSMNS $VACCESS -- ovs-vsctl add-br brint
+microk8s kubectl exec -n $OSMNS $VACCESS -- ifconfig net1 $VNFTUNIP/24
+microk8s kubectl exec -n $OSMNS $VACCESS -- ovs-vsctl add-port brint vxlanacc -- set interface vxlanacc type=vxlan options:remote_ip=$HOMETUNIP options:key=inet options:dst_port=8742
+microk8s kubectl exec -n $OSMNS $VACCESS -- ovs-vsctl add-port brint vxlanint -- set interface vxlanint type=vxlan options:remote_ip=$IPCPE options:key=inet options:dst_port=8742
+microk8s kubectl exec -n $OSMNS $VACCESS -- ip route add $IPCPE/32 via 169.254.1.1
 
-## 2. En VNF:vcpe agregar un bridge y asociar interfaces.
-sudo docker exec -it $VNF2 ovs-vsctl add-br br1
-sudo docker exec -it $VNF2 /sbin/ifconfig br1 $VCPEPRIVIP/24
-sudo docker exec -it $VNF2 ovs-vsctl add-port br1 vxlan1 -- set interface vxlan1 type=vxlan options:remote_ip=$IP11
-sudo docker exec -it $VNF2 ifconfig br1 mtu 1400
+## 4. En VNF:vcpe agregar un bridge y asociar interfaces
+echo "## 4. En VNF:vcpe agregar un bridge y asociar interfaces"
+microk8s kubectl exec -n $OSMNS $VCPE -- ovs-vsctl add-br brint
+microk8s kubectl exec -n $OSMNS $VCPE -- ifconfig brint $VCPEPRIVIP/24
+microk8s kubectl exec -n $OSMNS $VCPE -- ovs-vsctl add-port brint vxlanint -- set interface vxlanint type=vxlan options:remote_ip=$IPACCESS options:key=inet options:dst_port=8742
+microk8s kubectl exec -n $OSMNS $VCPE -- ifconfig brint mtu 1400
+microk8s kubectl exec -n $OSMNS $VCPE -- ip route add $IPACCESS/32 via 169.254.1.1
 
-## 3. En VNF:vcpe asignar dirección IP a interfaz de salida.
-sudo docker exec -it $VNF2 /sbin/ifconfig veth0 $VCPEPUBIP/24
-sudo docker exec -it $VNF2 ip route del 0.0.0.0/0 via 172.17.0.1
-sudo docker exec -it $VNF2 ip route add 0.0.0.0/0 via 10.2.3.254
+## 5. En VNF:vcpe asignar dirección IP a interfaz de salida
+echo "## 5. En VNF:vcpe asignar dirección IP a interfaz de salida"
+microk8s kubectl exec -n $OSMNS $VCPE -- ifconfig net1 $VCPEPUBIP/24
+microk8s kubectl exec -n $OSMNS $VCPE -- ip route del 0.0.0.0/0 via 169.254.1.1
+microk8s kubectl exec -n $OSMNS $VCPE -- ip route add 0.0.0.0/0 via $VCPEGW
 
-## 4. Iniciar Servidor DHCP 
-echo "--"
-echo "--DHCP Server Starting..."
+## 6. Iniciar Servidor DHCP
+echo "## 6. Iniciar Servidor DHCP"
 if [ -f "$DHCPDCONF" ]; then
     echo "--Using $DHCPDCONF for DHCP"
-    docker cp $DHCPDCONF $VNF2:/etc/dhcp/dhcpd.conf
+    microk8s kubectl -n $OSMNS cp $DHCPDCONF $VCPE:/etc/dhcp/dhcpd.conf
 else
     echo "--$DHCPCONF not found for DHCP, the container will use the default"
 fi
-sudo docker exec -it $VNF2 service isc-dhcp-server restart
+microk8s kubectl exec -n $OSMNS $VCPE -- sed -i 's/homeint/vxlan_sys_8742/' /etc/default/isc-dhcp-server
+microk8s kubectl exec -n $OSMNS $VCPE -- service isc-dhcp-server restart
 sleep 10
 
-## 5. En VNF:vcpe activar NAT para dar salida a Internet 
-docker cp /usr/bin/vnx_config_nat  $VNF2:/usr/bin
-sudo docker exec -it $VNF2 /usr/bin/vnx_config_nat br1 veth0
+## 7. En VNF:vcpe activar NAT para dar salida a Internet
+echo "## 7. En VNF:vcpe activar NAT para dar salida a Internet"
+microk8s kubectl exec -n $OSMNS $VCPE -- /usr/bin/vnx_config_nat brint net1
 
